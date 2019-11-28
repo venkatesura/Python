bf789f1365cc1e563c0be03270ea13dff406b707
venkatesura <venkateswara_sura@epam.com>
2019-11-28 16:50:23 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,13 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> 
+a=10
+>>> b=20
+>>> c=a*b
+>>> print(c)
+200
+>>> print(type(c))
+<class 'int'>
+>>> 
